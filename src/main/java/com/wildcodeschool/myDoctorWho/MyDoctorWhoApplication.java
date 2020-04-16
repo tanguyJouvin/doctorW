e50674c76f3b97cec195cf7ff3d880f4bc25896a
tanguy jouvin <tanguyJouvin@protonmail.com>
2020-04-16 16:12:55 +0200

    first commit

---
+++ src/main/java/com/wildcodeschool/myDoctorWho/MyDoctorWhoApplication.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.myDoctorWho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDoctorWhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyDoctorWhoApplication.class, args);
+	}
+
+}
